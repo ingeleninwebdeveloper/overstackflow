b3682a338efacdd3e3bdb2c0af62cfcd40062fc2
lenin fernando <ingeleninfernando@gmail.com>
2017-01-16 16:49:49 -0500

    implementa respuestas y cantidad de respuestas

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
 	has_secure_password validations: false
 
+	has_many :questions
+	has_many :answers
+
 
 	validates :email, uniqueness: true, format: /@/
 validates :password, presence: true, on: :create
