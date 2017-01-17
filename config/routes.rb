27032ece7e64128b9a2aeee1d3c38e1b5c597b7c
lenin fernando <ingeleninfernando@gmail.com>
2017-01-16 19:05:50 -0500

    implementa votos

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,9 @@ Rails.application.routes.draw do
 	resources :users, only: [:new, :create]
 	resources :questions do
 		resources :answers, only: [:create]
+		resource :vote, only: [:create, :destroy]
 	end
-	
+
 
 	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
